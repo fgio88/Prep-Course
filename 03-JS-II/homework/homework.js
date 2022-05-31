b7dfa39828e1a44c7f61730b5117928c5d221a05
Franco Giordano <fgiordanovp@gmail.com>
2022-05-30 21:47:35 -0300

    hwk

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -14,6 +14,12 @@ function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+
+  if (edad => 18) {
+    console.log('Allowed')
+  } else {
+    console.log('Not allowed')
+  }
 }
   
 function conection(status) {
